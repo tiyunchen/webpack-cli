77cf961af480ca2345ae406c2c06e5f5cf15da2b
chentiyun <15659263357cty>
2021-09-14 16:20:04 +0800

    fix: 删除代码

--- bin/yt.js
+++ bin/yt.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 const {spawn} = require('cross-spawn')
-const merge = require('webpack-merge');
 const path = require('path')
 const args = process.argv.slice(2);
 
 // process.cwd() 返回当前进程工作的目录
 const result = spawn.sync('node', [`${path.resolve(__dirname, '../lib/webpack.js')}`, ...args], { stdio: 'inherit' })
-// console.log(merge({a:2, b: [{a:2}]}, {a:1}))
